e94161d5a6f090caa15440197fe9945923bf21d7
Joe D'Amore <joe@joedamore.me>
2020-07-03 14:17:20 -0400

    Add background.js

---
+++ background.js
@@ -0,0 +1,66 @@
+/**
+ * Enables or disables the "Close Other Tabs" menu item according to tab state.
+ */
+const updateItemStatus = async function updateItemStatus() {
+  let enabled = true;
+  const allTabs = await browser.tabs.query({currentWindow: true});
+  if (allTabs.length <= 1) {
+    enabled = false;
+  }
+
+  browser.contextMenus.update("close_other", {
+    enabled,
+  });
+};
+
+/**
+ * Closes all tabs in `all`, except for the tab whose index matches `current`.
+ *
+ * @param {Object} current - Active tab.
+ * @param {Object[]} all - All tabs in current window.
+ */
+const closeTabs = function closeTabs(current, all) {
+  const removedTabIDs = all
+    .filter((tab) => {
+      return tab.index != current.index;
+    })
+    .filter((tab) => {
+      return !tab.pinned;
+    })
+    .map((tab) => {
+      return tab.id;
+    });
+
+  browser.tabs.remove(removedTabIDs);
+};
+
+// TODO Internationalization.
+const item = browser.contextMenus.create({
+  id: "close_other",
+  title: "Cl&ose Other Tabs",
+  contexts: ["tab"],
+}, () => {
+  if (browser.runtime.lastError) {
+    console.error(browser.runtime.lastError);
+  }
+});
+
+// Recalculate "Close Other Tabs" item status when tabs update.
+browser.tabs.onUpdated.addListener(() => {
+  updateItemStatus();
+});
+
+// Recalculate "Close Other Tabs" item status when tabs move.
+browser.tabs.onMoved.addListener(() => {
+  updateItemStatus();
+});
+
+/**
+ * Calls "closeTabs" if the "close_other" context menu item is clicked.
+ */
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === "close_other") {
+    const allTabs = await browser.tabs.query({currentWindow: true});
+    closeTabs(tab, allTabs);
+  }
+});
